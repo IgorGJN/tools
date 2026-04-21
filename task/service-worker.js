3839e3f3557b3dbc07f77ec691692d8a35d6f2ce
Igor Gabriel <106640138+IgorGJN@users.noreply.github.com>
2026-04-21 15:30:54 -0300

    v 4.0.1
    Tentativa de PWA

---
+++ task/service-worker.js
@@ -0,0 +1,69 @@
+const CACHE_NAME = "tasks-app-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+
+  "./css/tasks.css",
+  "./css/popup.css",
+  "./css/load.css",
+
+  "./js/tasks-data.js",
+  "./js/tasks-ui.js",
+  "./js/tasks-sync.js",
+  "./js/tasks-app.js",
+  "./js/tasks-calendar.js",
+  "./js/popup.js",
+
+  "./popup.html",
+
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// INSTALAÇÃO
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// ATIVAÇÃO (limpa cache antigo)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// FETCH (estratégia híbrida)
+self.addEventListener("fetch", (event) => {
+  // NÃO intercepta chamadas do Apps Script
+  if (event.request.url.includes("script.google.com")) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).then((networkResponse) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+      );
+    })
+  );
+});
